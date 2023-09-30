e037aca31f51911c95f6979b0adeaaf8749c1a29
Robiul-Haque <robiulcoc420@gmail.com>
2023-09-30 21:53:04 +0600

    update for vercel deployment

--- index.js
+++ index.js
@@ -54,10 +54,10 @@ const verifyToken = (req, res, next) => {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
         const summerCampSchoolDB = client.db("summer_camp_school");
         const userCollection = summerCampSchoolDB.collection("users");
